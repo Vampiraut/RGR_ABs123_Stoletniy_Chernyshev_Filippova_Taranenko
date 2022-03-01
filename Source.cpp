922e55c268fb9ae4a48a5c163872dc34a8226feb
Семен Столетний <noobas100@bk.ru>
2022-03-01 16:40:42 +0700

    remove update;

--- Source.cpp
+++ Source.cpp
@@ -25,67 +25,6 @@ bool PASSWORD_CHEK(bool chek_password)		//the function for password chek
 }
 
 
-
-
-void PASSWORD_request(int* password_array, int* flag_password)
-{
-	string password;
-	flag_password[0] = 1;
-	cout << "Enter password (if you don't have password -> write \"0\"): ";
-	cin >> password;
-	if (password == "0")
-	{
-		for (int i = 0; i < 30; i++)
-		{
-			int random_number = 1 + rand() % 9;
-			password_array[i] = random_number;
-		}
-	}
-	else
-	{
-		int i;
-		for (i = 0; password[i] != '\0'; i++)
-		{
-			if (flag_password[0] == 0)
-			{
-				break;
-			}
-			if (i == 30)
-			{
-				system("CLS");
-				cout << "Wrong password!" << endl;
-				flag_password[0] = 0;
-				break;
-			}
-			for (int j = 49; j < 58; j++)
-			{
-				char ASKII = j;
-				if (password[i] == ASKII)
-				{
-					password_array[i] = j - 48;
-					break;
-				}
-				else if (password[i] != ASKII && j == 57)
-				{
-					system("CLS");
-					cout << "Wrong password!" << endl;
-					flag_password[0] = 0;
-					break;
-				}
-			}
-		}
-		if (i != 30 && flag_password[0] != 0)
-		{
-			system("CLS");
-			cout << "Wrong password!" << endl;
-			flag_password[0] = 0;
-		}
-	}
-}
-
-
-
-
 void funktypename(string cryptotype)		//the function of writing the encryption type
 {
 	if (cryptotype == "1")
@@ -120,6 +59,7 @@ void funkprinciple(string funk)				//the function of writing the principle of op
 		cout << "Decryption" << endl;
 	}
 }	   
+
 
 //Code and Decode funk
 void Caesar_CODE(int smehenie)
@@ -275,186 +215,6 @@ void Caesar_DECODE(int smehenie)
 }
 
 
-
-
-
-void Gronsfeld_CODE(int* password_array)
-{
-	string inputstring;                                         //input string
-	string outputstring;										//output string
-	bool end;                                                   //cheсk-flag for continue
-
-	cout << "Enter coding string: ";                            //enter input string
-	cin.ignore();
-	getline(cin, inputstring);
-
-	int p = -1;
-	int smehenie = 0;
-
-	for (int i = 0; inputstring[i] != '\0'; i++)                //input string check
-	{
-
-		p++;
-		if (p == 29)
-		{
-			p = 0;
-		}
-		smehenie = password_array[p];
-
-		end = false;												//set check-flag to false
-		for (int j = 33; j < 127; j++)								//ASKII 33 -> 126 check in input string
-		{
-			char ASCIICod = j;											//int-dek to ASKII-symbol															
-			if (inputstring[i] == ASCIICod)								//comparison of the symbol of the input string and the ASKII character
-			{
-				int smena = j + smehenie;									//shift by ASKII table (smehenie (defalt = 3))
-				for (int h = 127; h < (127 + smehenie); h++)				//exclusion of unreadable characters from the ASKII table module
-				{
-					if (smena == h)
-					{
-						smena = smena + 65;
-					}
-				}
-				char ASCIICod_smena = smena;                                //changing a character and saving it to the output array
-				outputstring += ASCIICod_smena;
-				end = true;													//set check-flag to true
-				break;														//break the cycle
-			}
-		}
-		if (end == true)											//check check-flag (if true -> do not check symbols further)
-		{
-			continue;
-		}
-
-		for (int j = 192; j < 256; j++)								//ASKII 192 -> 255 check in input string
-		{
-			char ASCIICod = j;											//int-dek to ASKII-symbol
-			if (inputstring[i] == ASCIICod)								//comparison of the symbol of the input string and the ASKII character
-			{
-				int smena = j + smehenie;									//shift by ASKII table (smehenie (defalt = 3))
-				for (int h = 256; h < (256 + smehenie); h++)				//exclusion of unreadable characters from the ASKII table module
-				{
-					if (smena == h)
-					{
-						smena = smena - 256 + 33;
-					}
-				}
-				char ASCIICod_smena = smena;								//changing a character and saving it to the output array
-				outputstring += ASCIICod_smena;
-				end = true;													//set check-flag to true
-				break;														//break the cycle
-			}
-		}
-		if (end == true)											//check check-flag (if true -> do not check symbols further)
-		{
-			continue;
-		}
-		outputstring += inputstring[i];								//if there is no ASKII in the range -> copy them to the output array without modification
-	}
-
-	int n = outputstring.length();								//output string lenght
-
-	system("CLS");
-	funktypename("1");
-
-	cout << "Coding string: ";									//output of encrypted string
-	for (int g = 0; g < n; g++)
-	{
-		cout << outputstring[g];
-	}
-	cout << endl;
-}
-void Gronsfeld_DECODE(int* password_array)
-{
-	string inputstring;                                         //input string
-	string outputstring;										//output string
-	bool end;                                                   //cheсk-flag for continue
-
-	cout << "Enter decoding string: ";                          //enter input string
-	cin.ignore();
-	getline(cin, inputstring);
-
-	int p = -1;
-	int smehenie = 0;
-
-	for (int i = 0; inputstring[i] != '\0'; i++)                //input string check
-	{
-		p++;
-		if (p == 29)
-		{
-			p = 0;
-		}
-		smehenie = password_array[p];
-		end = false;												//set check-flag to false
-		for (int j = 33; j < 127; j++)								//ASKII 33 -> 126 check in input string
-		{
-			char ASCIICod = j;											//int-dek to ASKII-symbol														
-			if (inputstring[i] == ASCIICod)								//comparison of the symbol of the input string and the ASKII character
-			{
-				int smena = j - smehenie;									//shift by ASKII table (smehenie (default = 3))
-
-				for (int h = 33 - smehenie; h < 33; h++)					//exclusion of unreadable characters from the ASKII table module
-				{
-					if (smena == h)
-					{
-						smena = smena + 256 - 33;
-					}
-				}
-
-				char ASCIICod_smena = smena;                                //changing a character and saving it to the output array
-				outputstring += ASCIICod_smena;
-				end = true;													//set check-flag to true
-				break;														//break the cycle
-			}
-		}
-		if (end == true)											//check check-flag (if true -> do not check symbols further)
-		{
-			continue;
-		}
-
-		for (int j = 192; j < 256; j++)								//ASKII 192 -> 255 check in input string
-		{
-			char ASCIICod = j;											//int-dek to ASKII-symbol
-			if (inputstring[i] == ASCIICod)								//comparison of the symbol of the input string and the ASKII character
-			{
-				int smena = j - smehenie;									//shift by ASKII table (smehenie (default = 3))
-				for (int h = 192 - smehenie; h < 192; h++)					//exclusion of unreadable characters from the ASKII table module
-				{
-					if (smena == h)
-					{
-						smena = smena - 65;
-					}
-				}
-				char ASCIICod_smena = smena;								//changing a character and saving it to the output array
-				outputstring += ASCIICod_smena;
-				end = true;													//set check-flag to true
-				break;														//break the cycle
-			}
-		}
-		if (end == true)											//check check-flag (if true -> do not check symbols further)
-		{
-			continue;
-		}
-		outputstring += inputstring[i];								//if there is no ASKII in the range -> copy them to the output array without modification
-	}
-	int n = outputstring.length();								//output string lenght
-
-	system("CLS");
-	funktypename("1");
-
-	cout << "Decoding string: ";								//output of the decrypted string
-	for (int g = 0; g < n; g++)
-	{
-		cout << outputstring[g];
-	}
-	cout << endl;
-}
-
-
-
-
-
-
 //Main funk
 int main()
 {
@@ -534,11 +294,6 @@ int main()
 					Caesar_DECODE(smehenie);
 				}
 			}
-
-
-
-
-
 			else if (cryptotype == "2")
 			{
 				int smehenie = 5;	//Setting the offset for encoding (default = 3)
@@ -552,11 +307,6 @@ int main()
 					Caesar_DECODE(smehenie);
 				}
 			}
-
-
-
-
-
 			else if (cryptotype == "3")
 			{
 				cout << "3)" << endl;
