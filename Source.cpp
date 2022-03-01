c7db71d8c83dfcfe0599d4377f0f60ffa19966c3
Семен Столетний <noobas100@bk.ru>
2022-03-01 15:18:54 +0700

    Add Gronsfeld Code and Decode funk;

--- Source.cpp
+++ Source.cpp
@@ -268,6 +268,178 @@ void Caesar_DECODE(int smehenie)
 	cout << endl;
 }
 
+void Gronsfeld_CODE(int* password_array)
+{
+	string inputstring;                                         //input string
+	string outputstring;										//output string
+	bool end;                                                   //cheсk-flag for continue
+
+	cout << "Enter coding string: ";                            //enter input string
+	cin.ignore();
+	getline(cin, inputstring);
+
+	int p = -1;
+	int smehenie = 0;
+
+	for (int i = 0; inputstring[i] != '\0'; i++)                //input string check
+	{
+
+		p++;
+		if (p == 29)
+		{
+			p = 0;
+		}
+		smehenie = password_array[p];
+
+		end = false;												//set check-flag to false
+		for (int j = 33; j < 127; j++)								//ASKII 33 -> 126 check in input string
+		{
+			char ASCIICod = j;											//int-dek to ASKII-symbol															
+			if (inputstring[i] == ASCIICod)								//comparison of the symbol of the input string and the ASKII character
+			{
+				int smena = j + smehenie;									//shift by ASKII table (smehenie (defalt = 3))
+				for (int h = 127; h < (127 + smehenie); h++)				//exclusion of unreadable characters from the ASKII table module
+				{
+					if (smena == h)
+					{
+						smena = smena + 65;
+					}
+				}
+				char ASCIICod_smena = smena;                                //changing a character and saving it to the output array
+				outputstring += ASCIICod_smena;
+				end = true;													//set check-flag to true
+				break;														//break the cycle
+			}
+		}
+		if (end == true)											//check check-flag (if true -> do not check symbols further)
+		{
+			continue;
+		}
+
+		for (int j = 192; j < 256; j++)								//ASKII 192 -> 255 check in input string
+		{
+			char ASCIICod = j;											//int-dek to ASKII-symbol
+			if (inputstring[i] == ASCIICod)								//comparison of the symbol of the input string and the ASKII character
+			{
+				int smena = j + smehenie;									//shift by ASKII table (smehenie (defalt = 3))
+				for (int h = 256; h < (256 + smehenie); h++)				//exclusion of unreadable characters from the ASKII table module
+				{
+					if (smena == h)
+					{
+						smena = smena - 256 + 33;
+					}
+				}
+				char ASCIICod_smena = smena;								//changing a character and saving it to the output array
+				outputstring += ASCIICod_smena;
+				end = true;													//set check-flag to true
+				break;														//break the cycle
+			}
+		}
+		if (end == true)											//check check-flag (if true -> do not check symbols further)
+		{
+			continue;
+		}
+		outputstring += inputstring[i];								//if there is no ASKII in the range -> copy them to the output array without modification
+	}
+
+	int n = outputstring.length();								//output string lenght
+
+	system("CLS");
+	funktypename("1");
+
+	cout << "Coding string: ";									//output of encrypted string
+	for (int g = 0; g < n; g++)
+	{
+		cout << outputstring[g];
+	}
+	cout << endl;
+}
+void Gronsfeld_DECODE(int* password_array)
+{
+	string inputstring;                                         //input string
+	string outputstring;										//output string
+	bool end;                                                   //cheсk-flag for continue
+
+	cout << "Enter decoding string: ";                          //enter input string
+	cin.ignore();
+	getline(cin, inputstring);
+
+	int p = -1;
+	int smehenie = 0;
+
+	for (int i = 0; inputstring[i] != '\0'; i++)                //input string check
+	{
+		p++;
+		if (p == 29)
+		{
+			p = 0;
+		}
+		smehenie = password_array[p];
+		end = false;												//set check-flag to false
+		for (int j = 33; j < 127; j++)								//ASKII 33 -> 126 check in input string
+		{
+			char ASCIICod = j;											//int-dek to ASKII-symbol														
+			if (inputstring[i] == ASCIICod)								//comparison of the symbol of the input string and the ASKII character
+			{
+				int smena = j - smehenie;									//shift by ASKII table (smehenie (default = 3))
+
+				for (int h = 33 - smehenie; h < 33; h++)					//exclusion of unreadable characters from the ASKII table module
+				{
+					if (smena == h)
+					{
+						smena = smena + 256 - 33;
+					}
+				}
+
+				char ASCIICod_smena = smena;                                //changing a character and saving it to the output array
+				outputstring += ASCIICod_smena;
+				end = true;													//set check-flag to true
+				break;														//break the cycle
+			}
+		}
+		if (end == true)											//check check-flag (if true -> do not check symbols further)
+		{
+			continue;
+		}
+
+		for (int j = 192; j < 256; j++)								//ASKII 192 -> 255 check in input string
+		{
+			char ASCIICod = j;											//int-dek to ASKII-symbol
+			if (inputstring[i] == ASCIICod)								//comparison of the symbol of the input string and the ASKII character
+			{
+				int smena = j - smehenie;									//shift by ASKII table (smehenie (default = 3))
+				for (int h = 192 - smehenie; h < 192; h++)					//exclusion of unreadable characters from the ASKII table module
+				{
+					if (smena == h)
+					{
+						smena = smena - 65;
+					}
+				}
+				char ASCIICod_smena = smena;								//changing a character and saving it to the output array
+				outputstring += ASCIICod_smena;
+				end = true;													//set check-flag to true
+				break;														//break the cycle
+			}
+		}
+		if (end == true)											//check check-flag (if true -> do not check symbols further)
+		{
+			continue;
+		}
+		outputstring += inputstring[i];								//if there is no ASKII in the range -> copy them to the output array without modification
+	}
+	int n = outputstring.length();								//output string lenght
+
+	system("CLS");
+	funktypename("1");
+
+	cout << "Decoding string: ";								//output of the decrypted string
+	for (int g = 0; g < n; g++)
+	{
+		cout << outputstring[g];
+	}
+	cout << endl;
+}
+
 //Main funk
 int main()
 {
